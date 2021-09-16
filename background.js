7752229a9030584e5cc68d4cfb05565ba2abd1d3
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-09-16 16:30:18 -0400

    finished tutorial

---
+++ background.js
@@ -0,0 +1,20 @@
+chrome.runtime.onInstalled.addListener((details) => {
+  chrome.storage.local.set({
+    shows: [],
+  });
+  chrome.contextMenus.create({
+    title: 'Test Context Menu',
+    id: 'contextMenu1',
+    contexts: ['page', 'selection'],
+  });
+  chrome.contextMenus.onClicked.addListener((event) => {
+    fetch(`http://api.tvmaze.com/search/shows?q=${event.selectionText}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        chrome.storage.local.set({
+          shows: data,
+        });
+      });
+  });
+});
